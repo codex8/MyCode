6ea4aaa5631355d848d047ca905aabeb0e39e143
sam0hack <sam.nyx@live.com>
2015-01-14 14:27:52 +0530

    js

--- AjaxUsernamePasswordChecker/js/getresult.js
+++ AjaxUsernamePasswordChecker/js/getresult.js
@@ -7,53 +7,33 @@
 *
 ***/
 
-
 function lookup_users()
     {
 
    var get=document.getElementById('uname').value;
    var div=document.getElementById('pass').value;
 
-if(get.length==0)
-{
-$('#suggestions_cus').hide();   
-}
-else
-{
-            $("#suggestions_cus").show();
-                        $.ajax({ 
-                           url:'ajaxfile/getcon.php?get='+get+'&div='+div,
-                              success:function(data){
-                                $("#autoSuggestionsList_cus").html(data);
-                              }
-                        });
+                  if(get.length==0)
+                  {
+                  $('#suggestions_cus').hide();   
+                  }
+                  else
+                  {
+                              $("#suggestions_cus").show();
+                                          $.ajax({ 
+                                             url:'ajaxfile/getcon.php?get='+get+'&div='+div,
+                                                success:function(data){
+                                                  $("#autoSuggestionsList_cus").html(data);
+                                                }
+                                          });
 
-}
-
-
-}
-
-function fill2(valee)
-{
-var str="#";
+                  }
 
 
-$(str).val(valee);
-                    
-setTimeout("$('#suggestions_cus').hide();",200);   
-
-}
+                  }
 
 
 
-function getsrch() {
-     var subsrch="subsrch";
-//document.getElementById('myform1').submit();
-//alert(subsrch);
-return subsrch;
-
-
-}
 
 
 
